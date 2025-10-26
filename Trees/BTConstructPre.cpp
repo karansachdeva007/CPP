3a5f45c9fab28bc2eed5bcd78d2c314321cb9c77
Karan Sachdeva <149561993+karansachdeva007@users.noreply.github.com>
2025-10-26 17:21:18 +0530

    Add files via upload

---
+++ Trees/BTConstructPre.cpp
@@ -0,0 +1,40 @@
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+	int data;
+	Node* left;
+	Node* right;
+
+	Node(int val) {
+		data = val;
+		left = right = NULL;
+	}
+};
+static int index = -1;
+Node* buildTree(vector<int>preorder) {
+	index++;
+	if(preorder[index] == -1) {
+		return NULL;
+	}
+	Node* root = new Node(preorder[index]);
+	root->left = buildTree(preorder);
+	root->right = buildTree(preorder);
+	return root;
+}
+
+int main()
+{
+	vector<int>preorder = {1,2,-1,-1,3,4,-1,-1,5,-1,-1} ;
+	Node* root = buildTree(preorder);
+	cout<<root->data<<endl;
+	cout<<root->left->data<<endl;
+	cout<<root->right->data<<endl;
+	return 0;
+
+	return 0;
+}
